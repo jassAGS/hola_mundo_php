7b36dba87b75bd5d0cebf995ccee45210f56ab21
jassAGS <45088485+jassAGS@users.noreply.github.com>
2020-11-12 23:06:32 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php  $name = "Jassiel Andres";
+     $last = "Gutierrez sanchez";
+     $full = $name." ".$last;
+?>
+    
+   <?php  $name = "jassiel" ?>
+    <h1>
+        mi nombre es <?= $full?>
+    </h1>
+
+</body>
+</html>
